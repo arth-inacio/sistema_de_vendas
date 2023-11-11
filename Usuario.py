e78a2aed17b3a5a65955b91ab89bc90c9c25d1b4
Arthur <74980776+arth-inacio@users.noreply.github.com>
2023-11-11 18:40:22 -0300

    feat: Adicionando placeholders nos campos de login

--- Usuario.py
+++ Usuario.py
@@ -19,17 +19,19 @@ class Login:
         y = (tela_altura / 2) - (height / 2)
         self.loginwindow.geometry("%dx%d+%d+%d" % (width, height, x, y))
         self.loginwindow.resizable(0, 0)
+        self.username = StringVar(value="Nome Usuário")
+        self.password = StringVar(value="Senha Usuário")
         self.obj_login()
     
-    def obj_login(self):
-        self.loginframe = LabelFrame(self.loginwindow, width=600, height=300, bg="lightblue", bd=0)
+    def obj_login(self) -> None:
+        self.loginframe = LabelFrame(self.loginwindow, width=600, height=300, bg="#4267b2", bd=0)
         self.loginframe.place(x=200, y=200)
 
-        self.toplabel = Label(self.loginframe, fg="black", bg="lightblue", anchor="center", text="Login", font="roboto 20 bold underline")
+        self.toplabel = Label(self.loginframe, fg="black", bg="#4267b2", anchor="center", text="Login", font="roboto 20 bold underline")
         self.toplabel.place(x=150, y=20)
 
-        self.us = ttk.Entry(self.loginframe, width=30, font="roboto 12")
+        self.us = ttk.Entry(self.loginframe, textvariable=self.username, width=30, font="roboto 12")
         self.us.place(x=150, y=80, height=40)
 
-        self.pa = ttk.Entry(self.loginframe, width=30, show="*", font="roboto 12")
+        self.pa = ttk.Entry(self.loginframe, textvariable=self.password, width=30, show="*", font="roboto 12")
         self.pa.place(x=150, y=120, height=40)
